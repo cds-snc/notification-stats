1421db92dfebf0619a42aceecbe9839b766e3bdd
Steve Astels <sastels@gmail.com>
2020-08-20 10:15:52 -0400

    load services table too

--- application.py
+++ application.py
@@ -1,4 +1,7 @@
 import os
+import sys
+import datetime
+
 from dotenv import load_dotenv
 
 from flask import Flask
@@ -10,6 +13,7 @@ SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI", 'postgresql://pos
 
 application = Flask(__name__)
 application.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+
 db = SQLAlchemy(application)
 
 NOTIFICATION_TYPE = ['email', 'sms']
@@ -21,19 +25,48 @@ class NotificationHistory(db.Model):
 
     id = db.Column(UUID(as_uuid=True), primary_key=True)
     service_id = db.Column(UUID(as_uuid=True), db.ForeignKey('services.id'), index=True, unique=False)
-    # service = db.relationship('Service')
+    service = db.relationship('Service')
     notification_type = db.Column(notification_types, index=True, nullable=False)
     created_at = db.Column(db.DateTime, index=True, unique=False, nullable=False)
     sent_at = db.Column(db.DateTime, index=False, unique=False, nullable=True)
 
     def __repr__(self):
-        return '<Notification %r>' % self.id
+        return "<Not {}: {} {}>".format(self.created_at, self.service.name, self.notification_type )
+
+class Service(db.Model):
+    __tablename__ = 'services'
+
+    id = db.Column(UUID(as_uuid=True), primary_key=True)
+    name = db.Column(db.String(255), nullable=False, unique=True)
+    created_at = db.Column(
+        db.DateTime,
+        index=False,
+        unique=False,
+        nullable=False,
+        default=datetime.datetime.utcnow)
+    active = db.Column(db.Boolean, index=False, unique=False, nullable=False, default=True)
+    restricted = db.Column(db.Boolean, index=False, unique=False, nullable=False)
+    volume_sms = db.Column(db.Integer(), nullable=True, unique=False)
+    volume_email = db.Column(db.Integer(), nullable=True, unique=False)
+    count_as_live = db.Column(db.Boolean, nullable=False, default=True)
+    organisation_id = db.Column(UUID(as_uuid=True), db.ForeignKey('organisation.id'), index=True, nullable=True)
+    # organisation = db.relationship('Organisation', backref='services')
+
+    def __repr__(self):
+        return "<Service {} {}>".format(self.name, self.id)
 
 
 @application.route('/')
 def hello():
+    services = Service.query.all()
+    notifications = NotificationHistory.query.all()
+
+    print(services[:2], file=sys.stdout)
+    print(notifications[:5], file=sys.stdout)
+    sys.stdout.flush()
+
     try:
-        return("len: {}".format(len(NotificationHistory.query.all())))
+        return("Notifications: {} Services: {}".format(len(notifications), len(services)))
     except:
         return("Had an error connecting to database")
 
